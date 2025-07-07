b74c788973050bd0c4c530bb55d2771a0aed8a7d
Lester <ljamesceleste@gmail.com>
2025-07-07 19:17:03 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-# app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
