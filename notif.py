d073fc0971863f7c7f376bae8d5106d918428a48
Lester <ljamesceleste@gmail.com>
2025-07-15 19:23:42 +0800

    Initial Commit

--- notif.py
+++ notif.py
@@ -10,12 +10,10 @@ class Notification:
         self.sender_message = message
 
     def send_email(self):
-        email_message = (f"Subject:New Message\n\nName: {self.sender_name}\nEmail: {self.sender_email}"
-                         f"\nPhone: {self.sender_phone}\nMessage:{self.sender_message}")
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=os.getenv("MY_MAIL_ADDRESS"), password=os.getenv("MAIL_APP_PW"))
             connection.sendmail(from_addr=self.sender_email,
                                 to_addrs=os.getenv("MY_MAIL_ADDRESS"),
                                 msg=f"Subject:Blog Message from {self.sender_name} Email {self.sender_email} "
-                                    f"Phone {self.sender_phone}\n\nMessage:{self.sender_message}")
+                                    f"Phone {self.sender_phone}\n\n{self.sender_message}")
